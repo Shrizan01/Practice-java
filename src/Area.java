d8548f2a3c3fabb432b3b832d5cd28d69625638d
Shrizan01 <cresthashrizan6@gmail.com>
2024-09-05 19:49:24 +1000

    Area of Rectangle

---
+++ src/Area.java
@@ -0,0 +1,10 @@
+public class Area {
+        public static void main(String args[]){
+          float length = 4.5f;
+          float breadth = 5.33f;
+          float area;
+          String x = "cm";
+          area = length * breadth;
+            System.out.println(area + x);
+        }
+}
