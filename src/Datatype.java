12ec296516ece5499d208dede0a63e3090a177ae
Shreejan <cresthashrizan6@gmail.com>
2024-09-05 19:06:30 +1000

    final change

--- src/Datatype.java
+++ src/Datatype.java
@@ -1,7 +1,7 @@
 public class Datatype {
     public static void main(String[] args){
         int a = 200;
-        float b =12.01f;
+        float b = 12.01f;
         float c = a * b;
         char cur = '$';
         System.out.println("Total: " + c + cur);
