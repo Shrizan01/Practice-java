9316f81880a8351c402528a76a8cfe1658c930b0
Shrizan01 <cresthashrizan6@gmail.com>
2024-09-05 19:04:08 +1000

    CALCULATION

---
+++ src/Datatype.java
@@ -0,0 +1,9 @@
+public class Datatype {
+    public static void main(String[] args){
+        int a = 200;
+        float b =12.01f;
+        float c = a * b;
+        char cur = '$';
+        System.out.println("Total: " + c + cur);
+    }
+}
