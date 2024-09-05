58c35a0b9fa64c6a2821b957fd495de0b6c1566e
Shrizan01 <cresthashrizan6@gmail.com>
2024-09-05 19:58:36 +1000

    Final Change

--- src/AdditionOfNumbers.java
+++ src/AdditionOfNumbers.java
@@ -1,6 +1,6 @@
 public class Add {
     public static void main(String[] args){
-        int a = 10, b= 10, c = 10;
+        int a = 10, b = 10, c = 10;
         int z = a+b+c;
         System.out.println(z);
 
